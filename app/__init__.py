c4e1ab98c4c4f23f784beba07afb789a1942a8a5
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-05-16 22:56:10 +0300

    bugs on revert

--- app/__init__.py
+++ app/__init__.py
@@ -4,13 +4,13 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 
-bootstrap=Bootstrap()
+
 db = SQLAlchemy()
 csrf = CSRFProtect()
 
 def create_app(config_name):
     app = Flask(__name__)
-    
+    bootstrap=Bootstrap()
     ''''
     app configurations
     '''
