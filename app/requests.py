8ab6e0170da61a61f00167b3c824717a15fe726b
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-05-16 05:19:06 +0300

    return new quote in requests.py

--- app/requests.py
+++ app/requests.py
@@ -1,4 +1,19 @@
+from email.quoprimime import quote
 import urllib.request,json
+from webbrowser import get
+from app.models import Quote
 
 def request_quote(app):
-    pass
+    with urllib.request.urlopen('') as url:
+        data = url.read()
+        get_json = json.loads(data)
+        
+        if get_json['author'] and get_json['quote']:
+            author =get_json['author']
+            quote =get_json['quote']
+            
+            new_quote=Quote(author,quote)
+            
+            return new_quote
+        else:
+            return None
